fab9c916d363c48fa0d58757c08986229f3b0127
jihwan4720 <jihwan4720@gmail.com>
2021-09-17 11:13:11 +0900

    0917 front(btnlist) 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    axios.post("/api/users").then((response) => {
+      if (response.data) {
+        setUser(response.data);
+      } else {
+        alert("failed to");
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btnlist getbtn">GET</button>
+      <button className="btnlist postbtn">POST</button>
+      <button className="btnlist deletebtn">DELETE</button>
+      <button className="btnlist putbtn">PUT</button>
     </div>
   );
 }
