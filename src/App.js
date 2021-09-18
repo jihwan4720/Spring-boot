7072f55c806e2d8a8d6e8441f717e22035b21203
jihwan4720 <jihwan4720@gmail.com>
2021-09-18 23:42:00 +0900

    0918(진행중)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState({
+    id : 1,
+    name : '김유저',
+    age : 20
+  });
   useEffect(() => {
-    axios.post("/api/users").then((response) => {
+    axios.get("/api/users").then((response) => {
       if (response.data) {
+        const newuser = {
+          id : 1,
+        }
         setUser(response.data);
       } else {
         alert("failed to");
@@ -14,12 +21,28 @@ function App() {
     });
   }, []);
 
+  // useEffect(() => {
+  //   axios.post("/api/users").then((response) => {
+  //     if (response.data) {
+  //       setUser(response.data);
+  //     } else {
+  //       alert("failed to");
+  //     }
+  //   });
+  // }, []);
+
   return (
     <div className="App">
       <button className="btnlist getbtn">GET</button>
       <button className="btnlist postbtn">POST</button>
       <button className="btnlist deletebtn">DELETE</button>
       <button className="btnlist putbtn">PUT</button>
+
+       <div>
+        <p>아이디: {user.id}</p>
+        <p>이름 : {user.name}</p>
+        <p>나이 : {user.age}</p>
+      </div>
     </div>
   );
 }
